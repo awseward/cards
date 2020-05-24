e1bcca15d132351087436c10017d41fb7af78439
Andrew Seward <awswrd@gmail.com>
2020-05-24 12:36:03 -0700

    Do a fake noop write
    Have to get that RETURNING value back for the caller to not error. I
    don't love how this works, but it's fine for now.

--- src/models/user.nim
+++ src/models/user.nim
@@ -17,7 +17,14 @@ type User* = object
 proc initUser*(userId: string = $genOid()): User =
   let query = sql"""
 INSERT INTO users (user_id) VALUES (?)
-ON CONFLICT DO NOTHING
+-- This ON CONFLICT is a little odd, but it needs to give back the RETURNING
+-- block every time for the calling code to work correctly, which would not
+-- happen with ON CONFLICT DO NOTHING.
+--
+-- It might just be better to rework the way the caller does things, but this
+-- will do for now.
+--
+ON CONFLICT (user_id) DO UPDATE SET user_id = users.user_id
 RETURNING TO_JSON(DATE_TRUNC('SECOND', created_at))#>>'{}' AS created_at"""
   let conn = open("", "", "", databaseUrl)
   try:
