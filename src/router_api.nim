ae1cd383f73156a55316377bcbb4fadfbb8cc6b7
Andrew Seward <awswrd@gmail.com>
2020-05-24 13:55:58 -0700

    Ensure UserId sent in all API endpoints

--- src/router_api.nim
+++ src/router_api.nim
@@ -11,19 +11,29 @@ import ./models/game
 router api:
   template okText(thing: untyped): untyped =
     resp Http200, {"Content-Type": "text/plain"}, $thing
+
   template okJson(json: untyped): untyped =
     resp Http200, {"Content-Type": "application/json"}, $ %json
 
+  template ensureUserId(): string =
+    const key = "User-Id"
+    if not request.headers.hasKey key: halt Http401
+
+    request.headers["User-Id"]
+
   get "games":
+    let userId = ensureUserId()
     okJson getGames()
 
   post "games":
+    let userId = ensureUserId()
     let game = initGame()
     let jsonNode = %game
     jsonNode["status"] = %(getGameStatus game)
     okJson jsonNode
 
   get "game/@id":
+    let userId = ensureUserId()
     let gameOpt = findGame parseInt(@"id")
     if gameOpt.isSome():
       let game = gameOpt.get()
